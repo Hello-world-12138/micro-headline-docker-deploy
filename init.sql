75c67e8050b83119b670128dfc97281d5489cf5f
Hello-world-12138 <als20050228@qq.com>
2025-11-25 15:28:48 +0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -2,68 +2,53 @@ CREATE TABLE news_user (
     uid INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE,
     user_pwd VARCHAR(100),
-    nick_name VARCHAR(50)
+    nick_name VARCHAR(50),
+    version INT DEFAULT 1,
+    is_deleted INT DEFAULT 0
 ) CHARACTER SET utf8mb4;
 
 INSERT INTO news_user (username, user_pwd, nick_name) 
-VALUES ('zhangsan', 'e10adc3949ba59abbe56e057f20f883e', '张三');  -- 密码 123456 的 MD5
+VALUES ('zhangsan', 'e10adc3949ba59abbe56e057f20f883e', '张三'); -- 123456 的 MD5
 
-CREATE TABLE `sm_db1`.`Untitled`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `uid` int NOT NULL COMMENT '用户id（news_user.uid）',
-  `hid` int NOT NULL COMMENT '头条id（news_headline.hid）',
-  `browse_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后浏览时间',
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `uniq_uid_hid`(`uid` ASC, `hid` ASC) USING BTREE,
-  INDEX `idx_uid`(`uid` ASC) USING BTREE,
-  INDEX `idx_time`(`browse_time` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+CREATE TABLE news_type (
+    tid INT PRIMARY KEY AUTO_INCREMENT,
+    tname VARCHAR(10) NOT NULL,
+    version INT DEFAULT 1,
+    is_deleted INT DEFAULT 0
+) CHARACTER SET utf8mb4;
 
+INSERT INTO news_type (tname) VALUES ('新闻'),('体育'),('娱乐'),('科技'),('其他');
 
-CREATE TABLE `sm_db1`.`Untitled`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `hid` int NOT NULL COMMENT '头条id',
-  `uid` int NOT NULL COMMENT '评论用户id',
-  `content` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '评论内容',
-  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '评论时间',
-  `is_deleted` int NULL DEFAULT 0 COMMENT '逻辑删除',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `idx_hid`(`hid` ASC) USING BTREE,
-  INDEX `idx_uid`(`uid` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+CREATE TABLE news_headline (
+    hid INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50) NOT NULL,
+    article VARCHAR(5000) NOT NULL,
+    type INT NOT NULL,
+    publisher INT NOT NULL,
+    page_views INT DEFAULT 0,
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    version INT DEFAULT 1,
+    is_deleted INT DEFAULT 0
+) CHARACTER SET utf8mb4;
 
+CREATE TABLE news_comment (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    hid INT NOT NULL,
+    uid INT NOT NULL,
+    content VARCHAR(500) NOT NULL,
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    is_deleted INT DEFAULT 0,
+    INDEX idx_hid (hid),
+    INDEX idx_uid (uid)
+) CHARACTER SET utf8mb4;
 
-CREATE TABLE `sm_db1`.`Untitled`  (
-  `hid` int NOT NULL AUTO_INCREMENT COMMENT '头条id',
-  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '头条标题',
-  `article` varchar(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '头条新闻内容',
-  `type` int NOT NULL COMMENT '头条类型id',
-  `publisher` int NOT NULL COMMENT '头条发布用户id',
-  `page_views` int NOT NULL COMMENT '头条浏览量',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '头条发布时间',
-  `update_time` datetime NULL DEFAULT NULL COMMENT '头条最后的修改时间',
-  `version` int NULL DEFAULT 1 COMMENT '乐观锁',
-  `is_deleted` int NULL DEFAULT 0 COMMENT '头条是否被删除 1 删除  0 未删除',
-  PRIMARY KEY (`hid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 24 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
-
-CREATE TABLE `sm_db1`.`Untitled`  (
-  `tid` int NOT NULL AUTO_INCREMENT COMMENT '新闻类型id',
-  `tname` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '新闻类型描述',
-  `version` int NULL DEFAULT 1 COMMENT '乐观锁',
-  `is_deleted` int NULL DEFAULT 0 COMMENT '头条是否被删除 1 删除  0 未删除',
-  PRIMARY KEY (`tid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
-
-CREATE TABLE `sm_db1`.`Untitled`  (
-  `uid` int NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户登录名',
-  `user_pwd` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户登录密码密文',
-  `nick_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户昵称',
-  `version` int NULL DEFAULT 1 COMMENT '乐观锁',
-  `is_deleted` int NULL DEFAULT 0 COMMENT '头条是否被删除 1 删除  0 未删除',
-  PRIMARY KEY (`uid`) USING BTREE,
-  UNIQUE INDEX `username_unique`(`username` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+CREATE TABLE browse_history (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    uid INT NOT NULL,
+    hid INT NOT NULL,
+    browse_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UNIQUE INDEX uniq_uid_hid (uid, hid),
+    INDEX idx_uid (uid),
+    INDEX idx_time (browse_time)
+) CHARACTER SET utf8mb4;
